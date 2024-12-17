b277be744e92db1efddc609a86aa4438d5315e98
Aleksander Markowicz <aleksnder.m.2k2@gmail.com>
2024-12-17 03:15:00 +0100

    LED matrix and OLED UI: integration and testing

--- LED_Panel.ino
+++ LED_Panel.ino
@@ -1,20 +1,230 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <Adafruit_NeoPixel.h>
 
-// LED setup
-#define LED_PIN     6
-#define NUM_LEDS    48
+// Ustawienia wyświetlacza OLED
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define OLED_I2C_ADDRESS 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+// Czcionki
+#include <Fonts/FreeMono9pt7b.h>
+#include <Fonts/FreeMono12pt7b.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+#include <Fonts/FreeMonoBold12pt7b.h>
+
+// Ustawienia macierzy LED
+#define LED_PIN 6
+#define NUM_LEDS 48
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, LED_PIN, NEO_GRB + NEO_KHZ800);
 
+// Piny enkodera obrotowego
+#define PIN_A 2
+#define PIN_B 3
+#define PIN_SW 4
+
+// Zmienne enkodera
+volatile int encoderPosition = 0;
+volatile bool encoderMoved = false;
+volatile bool buttonPressed = false;
+
+// Tryby
+enum Mode { TEMP, POWER, HUE }; 
+Mode currentMode = POWER;        
+bool valueSelected = true;   
+
+// Zmienne wartości
+int kelvin = 3500;              
+int powerPercentage = 35;       
+int hue = 180;                  
+
+// Czułość enkodera
+const int ENCODER_SENSITIVITY = 4; 
+unsigned long lastButtonPress = 0;
+const unsigned long debounceDelay = 200;
+
+// Funkcja konwertująca temperaturę barwową na RGB
+uint32_t kelvinToColor(int temp) {
+  int r, g, b;
+  temp = constrain(temp, 2900, 5600);
+
+  if (temp <= 4000) { 
+    // Niska temperatura: ciepłe barwy (pomarańcz, czerwony)
+    r = 255;
+    g = map(temp, 2900, 4000, 50, 165); // Zielony kanał przechodzi od małej do średniej intensywności
+    b = map(temp, 2900, 4000, 0, 50);  // Niebieski kanał minimalny
+  } else { 
+    // Wysoka temperatura: zimne barwy (białe, lekko niebieskie)
+    r = map(temp, 4000, 5600, 255, 200); // Czerwony kanał lekko spada
+    g = 255;                            // Zielony kanał utrzymuje pełną intensywność
+    b = map(temp, 4000, 5600, 50, 200); // Niebieski kanał rośnie do większej wartości
+  }
+  return strip.Color(r, g, b);
+}
+
+// Funkcja ustawiająca jasność i kolor LED
+void updateLEDs() {
+  uint32_t color;
+
+  if (currentMode == HUE) {
+    color = strip.ColorHSV(hue * 182, 255, 255);
+  } else {
+    color = kelvinToColor(kelvin);
+  }
+
+  int brightness = map(powerPercentage, 0, 100, 0, 255);
+  strip.setBrightness(brightness);
+
+  for (int i = 0; i < NUM_LEDS; i++) {
+    strip.setPixelColor(i, color);
+  }
+  strip.show();
+}
+
+// Funkcja rysująca wartość trybu
+void drawValue(int x, int y, Mode mode, bool highlight) {
+  display.setFont(&FreeMono12pt7b);
+  display.setTextSize(1);
+  char valueText[10];
+  switch (mode) {
+    case TEMP:
+      sprintf(valueText, "%dK", kelvin);
+      break;
+    case POWER:
+      sprintf(valueText, "%d%%", powerPercentage);
+      break;
+    case HUE:
+      sprintf(valueText, "%d", hue);
+      break;
+  }
+
+  if (highlight) {
+    int16_t x1, y1;
+    uint16_t w, h;
+    display.setFont(&FreeMonoBold12pt7b);
+    display.getTextBounds(valueText, x, y, &x1, &y1, &w, &h);
+    display.fillRect(x1 - 1, y1 - 1, w + 2, h + 2, SSD1306_WHITE);
+    display.setTextColor(SSD1306_BLACK);
+    display.setCursor(x, y);
+    display.print(valueText);
+    display.setTextColor(SSD1306_WHITE);
+  } else {
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(x, y);
+    display.print(valueText);
+  }
+}
+
+// Funkcja rysująca nazwę trybu
+void drawMode(int x, int y, Mode mode, bool highlight) {
+  display.setFont(&FreeMono9pt7b);
+  display.setTextSize(1);
+
+  const char *modeText;
+  switch (mode) {
+    case TEMP: modeText = "TEMP"; break;
+    case POWER: modeText = "POWER"; break;
+    case HUE: modeText = "HUE"; break;
+  }
+
+  if (highlight) {
+    int16_t x1, y1;
+    uint16_t w, h;
+    display.setFont(&FreeMonoBold9pt7b);
+    display.getTextBounds(modeText, x, y, &x1, &y1, &w, &h);
+    display.fillRect(x1 - 1, y1 - 1, w + 2, h + 2, SSD1306_WHITE);
+    display.setTextColor(SSD1306_BLACK);
+    display.setCursor(x, y);
+    display.print(modeText);
+    display.setTextColor(SSD1306_WHITE);
+  } else {
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(x, y);
+    display.print(modeText);
+  }
+}
+
+// Rysowanie UI
+void drawUI() {
+  display.clearDisplay();
+  drawMode(0, 11, currentMode, !valueSelected);
+  drawValue(0, 30, currentMode, valueSelected);
+  display.display();
+}
+
+void updateEncoder() {
+  int MSB = digitalRead(PIN_A);
+  int LSB = digitalRead(PIN_B);
+  int encoded = (MSB << 1) | LSB;
+  static int lastEncoded = 0;
+  static const int8_t encoderTable[16] = {
+    0, -1,  1,  0,
+    1,  0,  0, -1,
+   -1,  0,  0,  1,
+    0,  1, -1,  0
+  };
+  encoderPosition += encoderTable[((lastEncoded << 2) | encoded) & 0x0F];
+  encoderMoved = true;
+  lastEncoded = encoded;
+}
+
+void handleButton() {
+  unsigned long currentTime = millis();
+  if ((currentTime - lastButtonPress) > debounceDelay) {
+    buttonPressed = true;
+    lastButtonPress = currentTime;
+  }
+}
+
 void setup() {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDRESS)) {
+    for (;;);
+  }
+  display.clearDisplay();
+
+  pinMode(PIN_A, INPUT_PULLUP);
+  pinMode(PIN_B, INPUT_PULLUP);
+  pinMode(PIN_SW, INPUT_PULLUP);
+
+  attachInterrupt(digitalPinToInterrupt(PIN_A), updateEncoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PIN_B), updateEncoder, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(PIN_SW), handleButton, FALLING);
+
   strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  strip.show();
+  updateLEDs();
+  drawUI();
 }
 
 void loop() {
-  // Ustawienie pełnej jasności na kolor czerwony (można zmienić na inny)
-  for (int i = 0; i < NUM_LEDS; i++) {
-    strip.setPixelColor(i, strip.Color(255, 255, 255)); // Czerwony kolor
+  if (buttonPressed) {
+    buttonPressed = false;
+    valueSelected = !valueSelected;
+    drawUI();
   }
-  strip.setBrightness(255); // Ustawienie maksymalnej jasności
-  strip.show(); // Zaktualizowanie stanu diod
+
+  if (encoderMoved) {
+    encoderMoved = false;
+    int effectiveChange = encoderPosition / ENCODER_SENSITIVITY; 
+    encoderPosition %= ENCODER_SENSITIVITY;
+
+    if (valueSelected) {
+      if (currentMode == POWER) {
+        powerPercentage += effectiveChange * 5;
+        powerPercentage = constrain(powerPercentage, 0, 100);
+      } else if (currentMode == HUE) {
+        hue += effectiveChange * 5;
+        hue = (hue + 360) % 360; 
+      } else if (currentMode == TEMP) {
+        kelvin += effectiveChange * 100;
+        kelvin = constrain(kelvin, 2900, 5600);
+      }
+      updateLEDs();
+    } else {
+      currentMode = static_cast<Mode>((currentMode + effectiveChange + 3) % 3);
+    }
+    drawUI();
+  }
 }
