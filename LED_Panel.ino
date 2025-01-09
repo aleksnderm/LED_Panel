0cdf686461818905f19832fc18a9e764c93db9a1
Aleksander Markowicz <aleksnder.m.2k2@gmail.com>
2025-01-09 17:34:19 +0100

    Added dynamic mode handling and EffectsMode integration

--- LED_Panel.ino
+++ LED_Panel.ino
@@ -2,6 +2,21 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_NeoPixel.h>
+#include "src/Mode.h"
+#include "src/EffectMode.h"
+
+// Ustawienia i zmienne globalne
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 32
+#define OLED_I2C_ADDRESS 0x3C
+#define LED_PIN 6
+#define NUM_LEDS 48
+
+// Deklaracja funkcji
+void updateLEDs();
+void drawUI();
+void updateEncoder();
+void handleButton();
 
 // Ustawienia wyświetlacza OLED
 #define SCREEN_WIDTH 128
@@ -31,17 +46,17 @@ volatile bool encoderMoved = false;
 volatile bool buttonPressed = false;
 
 // Tryby
-enum Mode { TEMP, POWER, HUE }; 
-Mode currentMode = TEMP;        
-bool valueSelected = true;   
+am::Mode* modes[4]; // Tablica wskaźników do trybów
+int currentModeIndex = 3; // Indeks aktualnego trybu
+bool valueSelected = true;
 
-// Zmienne wartości
-int kelvin = 3500;              
-int powerPercentage = 35;       
-int hue = 180;                  
+// Funkcja ustawiająca jasność i kolor LED
+void updateLEDs() {
+  modes[currentModeIndex]->execute(strip); // Wywołanie aktualnego trybu
+}
 
 // Czułość enkodera
-const int ENCODER_SENSITIVITY = 4; 
+const int ENCODER_SENSITIVITY = 4;
 unsigned long lastButtonPress = 0;
 const unsigned long debounceDelay = 200;
 
@@ -49,58 +64,11 @@ const unsigned long debounceDelay = 200;
 int batteryLevel = 85;  // Poziom baterii (0-100)
 bool bluetoothConnected = true;  // Status Bluetooth (połączony: true / niepołączony: false)
 
-// Funkcja konwertująca temperaturę barwową na RGB
-uint32_t kelvinToColor(int temp) {
-  int r, g, b;
-  temp = constrain(temp, 2900, 5600);
-
-  if (temp <= 4000) { 
-    r = 255;
-    g = map(temp, 2900, 4000, 50, 165); 
-    b = map(temp, 2900, 4000, 0, 50);  
-  } else { 
-    r = map(temp, 4000, 5600, 255, 200); 
-    g = 255;                            
-    b = map(temp, 4000, 5600, 50, 200); 
-  }
-  return strip.Color(r, g, b);
-}
-
-// Funkcja ustawiająca jasność i kolor LED
-void updateLEDs() {
-  uint32_t color;
-
-  if (currentMode == HUE) {
-    color = strip.ColorHSV(hue * 182, 255, 255);
-  } else {
-    color = kelvinToColor(kelvin);
-  }
-
-  int brightness = map(powerPercentage, 0, 100, 0, 255);
-  strip.setBrightness(brightness);
-
-  for (int i = 0; i < NUM_LEDS; i++) {
-    strip.setPixelColor(i, color);
-  }
-  strip.show();
-}
-
 // Funkcja rysująca wartość trybu
-void drawValue(int x, int y, Mode mode, bool highlight) {
+void drawValue(int x, int y, am::Mode* mode, bool highlight) {
   display.setFont(&FreeMono12pt7b);
   display.setTextSize(1);
-  char valueText[10];
-  switch (mode) {
-    case TEMP:
-      sprintf(valueText, "%dK", kelvin);
-      break;
-    case POWER:
-      sprintf(valueText, "%d%%", powerPercentage);
-      break;
-    case HUE:
-      sprintf(valueText, "%d", hue);
-      break;
-  }
+  const char* valueText = mode->getDisplayValue(); // Pobranie wartości trybu
 
   if (highlight) {
     int16_t x1, y1;
@@ -120,16 +88,11 @@ void drawValue(int x, int y, Mode mode, bool highlight) {
 }
 
 // Funkcja rysująca nazwę trybu
-void drawMode(int x, int y, Mode mode, bool highlight) {
+void drawMode(int x, int y, am::Mode* mode, bool highlight) {
   display.setFont(&FreeMono9pt7b);
   display.setTextSize(1);
 
-  const char *modeText;
-  switch (mode) {
-    case TEMP: modeText = "TEMP"; break;
-    case POWER: modeText = "POWER"; break;
-    case HUE: modeText = "HUE"; break;
-  }
+  const char* modeText = mode->getName(); // Pobranie nazwy trybu
 
   if (highlight) {
     int16_t x1, y1;
@@ -151,7 +114,7 @@ void drawMode(int x, int y, Mode mode, bool highlight) {
 // Funkcja rysująca baterię
 void drawBattery(int x, int y, int level) {
   display.drawRect(x, y, 9, 13, SSD1306_WHITE);
-  display.fillRect(x+2, y-3 , 5, 2, SSD1306_WHITE);
+  display.fillRect(x + 2, y - 3, 5, 2, SSD1306_WHITE);
   int fillWidth = map(level, 0, 100, 1, 11);
   display.fillRect(x + 2, y + 2, 5, fillWidth, SSD1306_WHITE);
 }
@@ -164,11 +127,11 @@ void drawBluetooth(int x, int y) {
   display.print("BT");
 }
 
-// Rysowanie UI
+// Funkcja rysująca UI
 void drawUI() {
   display.clearDisplay();
-  drawMode(0, 11, currentMode, !valueSelected);
-  drawValue(0, 30, currentMode, valueSelected);
+  drawMode(0, 11, modes[currentModeIndex], !valueSelected);
+  drawValue(0, 30, modes[currentModeIndex], valueSelected);
   drawBattery(SCREEN_WIDTH - 9, 19, batteryLevel);
   drawBluetooth(SCREEN_WIDTH - 21, 11);
   display.display();
@@ -203,6 +166,7 @@ void handleButton() {
 }
 
 void setup() {
+  Serial.begin(9600);
   if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_I2C_ADDRESS)) {
     for (;;);
   }
@@ -218,38 +182,45 @@ void setup() {
 
   strip.begin();
   strip.show();
-  updateLEDs();
-  drawUI();
+
+  // Inicjalizacja trybów
+  modes[0] = new am::TempMode();
+  modes[1] = new am::HueMode();
+  modes[2] = new am::PowerMode();
+  modes[3] = new am::EffectsMode();
+
+  updateLEDs(); // Ustawienie LED na start
+  drawUI();     // Wyświetlenie interfejsu na start
 }
+
+
+
+
 
 void loop() {
+  // Obsługa przycisku - zmiana trybu
   if (buttonPressed) {
     buttonPressed = false;
-    valueSelected = !valueSelected;
+    currentModeIndex = (currentModeIndex + 1) % 4; // Zmiana trybu (4 tryby: Temp, Hue, Power, Effects)
     drawUI();
   }
 
+  // Obsługa enkodera - dostosowanie wartości w bieżącym trybie
   if (encoderMoved) {
     encoderMoved = false;
-    int effectiveChange = encoderPosition / ENCODER_SENSITIVITY; 
+    int effectiveChange = encoderPosition / ENCODER_SENSITIVITY;
     encoderPosition %= ENCODER_SENSITIVITY;
 
-    if (valueSelected) {
-      if (currentMode == TEMP) {
-        kelvin += effectiveChange * 100;
-        kelvin = constrain(kelvin, 2900, 5600);
-      } else if (currentMode == POWER) {
-        powerPercentage += effectiveChange;
-        powerPercentage = constrain(powerPercentage, 0, 100);
-      } else if (currentMode == HUE) {
-        hue += effectiveChange;
-        hue = constrain(hue, 0, 255);
-      }
-    } else {
-      currentMode = static_cast<Mode>((currentMode + effectiveChange + 3) % 3);
-    }
-
-    updateLEDs();
+    modes[currentModeIndex]->adjust(effectiveChange); // Dostosowanie wartości trybu
     drawUI();
   }
+
+  // Obsługa trybu efektów (EffectsMode)
+  if (currentModeIndex == 3) { // Indeks 3 to EffectsMode
+    modes[currentModeIndex]->execute(strip); // Wywołanie pętli efektów
+  } else {
+    // Jednorazowe wykonanie dla pozostałych trybów
+    modes[currentModeIndex]->execute(strip);
+  }
 }
+
